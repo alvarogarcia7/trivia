ccd4491b33e407317ac7710eefcd0feb9ecb0467
Alvaro Garcia <derecerca@gmail.com>
2014-11-09 06:53:00 +0100

    added a new game, coverage is now 100%

--- java/src/test/java/com/adaptionsoft/games/trivia/GameAcceptanceTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameAcceptanceTest.java
@@ -75,6 +75,36 @@ public class GameAcceptanceTest {
 	}
 	
 	@Test
+	public void acceptanceTestWithSeed3() {
+		MockSystemOutput mockOutput = MockSystemOutput.inject();
+		
+		Game aGame = new Game();
+
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue"); 
+
+		Random rand = new Random(3L);   
+
+		boolean notAWinner;
+		do {
+
+			aGame.roll(rand.nextInt(5) + 1);
+
+			if (rand.nextInt(9) == 7) {
+				notAWinner = aGame.wrongAnswer();
+			} else {
+				notAWinner = aGame.wasCorrectlyAnswered();
+			}
+
+		} while (notAWinner);
+
+		System.err.println(mockOutput.toString()); 
+
+		AssertWithoutEndOfLine.equals(expectedOutputForGameWithSeed3(), mockOutput.toString());
+	}
+	
+	@Test
 	public void isPlayableCoverageTest() {
 		Game aGame = new Game();
 		
@@ -85,6 +115,124 @@ public class GameAcceptanceTest {
 		assertTrue(aGame.isPlayable());
 	}
 
+	private String expectedOutputForGameWithSeed3() {
+		return "Chet was added\n" +
+				"They are player number 1\n" +
+				"Pat was added\n" +
+				"They are player number 2\n" +
+				"Sue was added\n" +
+				"They are player number 3\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 5\n" +
+				"Chet's new location is 5\n" +
+				"The category is Science\n" +
+				"Science Question 0\n" +
+				"Answer was corrent!!!!\n" +
+				"Chet now has 1 Gold Coins.\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 1\n" +
+				"Pat's new location is 1\n" +
+				"The category is Science\n" +
+				"Science Question 1\n" +
+				"Question was incorrectly answered\n" +
+				"Pat was sent to the penalty box\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 4\n" +
+				"Sue's new location is 4\n" +
+				"The category is Pop\n" +
+				"Pop Question 0\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 1 Gold Coins.\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 5\n" +
+				"Chet's new location is 10\n" +
+				"The category is Sports\n" +
+				"Sports Question 0\n" +
+				"Question was incorrectly answered\n" +
+				"Chet was sent to the penalty box\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 5\n" +
+				"Pat is getting out of the penalty box\n" +
+				"Pat's new location is 6\n" +
+				"The category is Sports\n" +
+				"Sports Question 1\n" +
+				"Answer was correct!!!!\n" +
+				"Pat now has 1 Gold Coins.\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 1\n" +
+				"Sue's new location is 5\n" +
+				"The category is Science\n" +
+				"Science Question 2\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 2 Gold Coins.\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 3\n" +
+				"Chet is getting out of the penalty box\n" +
+				"Chet's new location is 1\n" +
+				"The category is Science\n" +
+				"Science Question 3\n" +
+				"Answer was correct!!!!\n" +
+				"Chet now has 2 Gold Coins.\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 2\n" +
+				"Pat is not getting out of the penalty box\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 2\n" +
+				"Sue's new location is 7\n" +
+				"The category is Rock\n" +
+				"Rock Question 0\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 3 Gold Coins.\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 2\n" +
+				"Chet is not getting out of the penalty box\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 2\n" +
+				"Pat is not getting out of the penalty box\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 1\n" +
+				"Sue's new location is 8\n" +
+				"The category is Pop\n" +
+				"Pop Question 1\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 4 Gold Coins.\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 4\n" +
+				"Chet is not getting out of the penalty box\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 3\n" +
+				"Pat is getting out of the penalty box\n" +
+				"Pat's new location is 9\n" +
+				"The category is Science\n" +
+				"Science Question 4\n" +
+				"Answer was correct!!!!\n" +
+				"Pat now has 2 Gold Coins.\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 2\n" +
+				"Sue's new location is 10\n" +
+				"The category is Sports\n" +
+				"Sports Question 2\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 5 Gold Coins.\n" +
+				"Chet is the current player\n" +
+				"They have rolled a 4\n" +
+				"Chet is not getting out of the penalty box\n" +
+				"Pat is the current player\n" +
+				"They have rolled a 3\n" +
+				"Pat is getting out of the penalty box\n" +
+				"Pat's new location is 0\n" +
+				"The category is Pop\n" +
+				"Pop Question 2\n" +
+				"Answer was correct!!!!\n" +
+				"Pat now has 3 Gold Coins.\n" +
+				"Sue is the current player\n" +
+				"They have rolled a 3\n" +
+				"Sue's new location is 1\n" +
+				"The category is Science\n" +
+				"Science Question 5\n" +
+				"Answer was corrent!!!!\n" +
+				"Sue now has 6 Gold Coins.\n";
+	}
 	private String expectedOutputForGameWithSeed2() {
 		return 	"Chet was added\n" +
 				"They are player number 1\n" +
